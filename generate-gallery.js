4e54546bc5be8e4b4eceb03cfa141210973f935a
SkSix <sukesh.kawa@gmail.com>
2025-05-30 20:57:59 +0530

    No duplicate files
    On Vercel (when you deploy):
    Uses the includeFiles configuration from vercel.json
    Copies all necessary files (images, HTML, CSS, JS) to the deployment
    Serves files from their original locations
    Uses the generated gallery HTML from dist/gallery-items.html

--- generate-gallery.js
+++ generate-gallery.js
@@ -60,83 +60,9 @@ function generateGalleryHTML() {
         fs.mkdirSync(distDir, { recursive: true });
     }
 
-    // Copy image directory to dist
-    const imageDir = path.join(__dirname, 'image');
-    const imageDest = path.join(distDir, 'image');
-    if (fs.existsSync(imageDir)) {
-        fs.mkdirSync(imageDest, { recursive: true });
-        const subFiles = fs.readdirSync(imageDir);
-        subFiles.forEach(subFile => {
-            const subSrcPath = path.join(imageDir, subFile);
-            const subDestPath = path.join(imageDest, subFile);
-            
-            if (fs.lstatSync(subSrcPath).isDirectory()) {
-                // Copy directory contents
-                fs.mkdirSync(subDestPath, { recursive: true });
-                const subSubFiles = fs.readdirSync(subSrcPath);
-                subSubFiles.forEach(subSubFile => {
-                    const subSubSrcPath = path.join(subSrcPath, subSubFile);
-                    const subSubDestPath = path.join(subDestPath, subSubFile);
-                    
-                    if (fs.lstatSync(subSubSrcPath).isDirectory()) {
-                        // Copy sub-sub directories
-                        fs.mkdirSync(subSubDestPath, { recursive: true });
-                        const subSubSubFiles = fs.readdirSync(subSubSrcPath);
-                        subSubSubFiles.forEach(subSubSubFile => {
-                            const subSubSubSrcPath = path.join(subSubSrcPath, subSubSubFile);
-                            const subSubSubDestPath = path.join(subSubDestPath, subSubSubFile);
-                            
-                            if (fs.lstatSync(subSubSubSrcPath).isDirectory()) {
-                                // Copy sub-sub-sub directories
-                                fs.mkdirSync(subSubSubDestPath, { recursive: true });
-                                const subSubSubSubFiles = fs.readdirSync(subSubSubSrcPath);
-                                subSubSubSubFiles.forEach(subSubSubSubFile => {
-                                    const subSubSubSubSrcPath = path.join(subSubSubSrcPath, subSubSubSubFile);
-                                    const subSubSubSubDestPath = path.join(subSubSubDestPath, subSubSubSubFile);
-                                    fs.copyFileSync(subSubSubSubSrcPath, subSubSubSubDestPath);
-                                });
-                            } else {
-                                // Copy file
-                                fs.copyFileSync(subSubSubSrcPath, subSubSubDestPath);
-                            }
-                        });
-                    } else {
-                        // Copy file
-                        fs.copyFileSync(subSubSrcPath, subSubDestPath);
-                    }
-                });
-            } else {
-                // Copy file
-                fs.copyFileSync(subSrcPath, subDestPath);
-            }
-        });
-    }
-
-    // Copy other files to dist directory
-    const files = fs.readdirSync(__dirname);
-    files.forEach(file => {
-        if (file !== 'dist' && !file.startsWith('.') && file !== 'image') {
-            const srcPath = path.join(__dirname, file);
-            const destPath = path.join(distDir, file);
-            
-            if (fs.lstatSync(srcPath).isDirectory()) {
-                fs.mkdirSync(destPath, { recursive: true });
-                const subFiles = fs.readdirSync(srcPath);
-                subFiles.forEach(subFile => {
-                    const subSrcPath = path.join(srcPath, subFile);
-                    const subDestPath = path.join(destPath, subFile);
-                    fs.copyFileSync(subSrcPath, subDestPath);
-                });
-            } else {
-                fs.copyFileSync(srcPath, destPath);
-            }
-        }
-    });
-    
     // Write gallery-items.html to dist directory
     fs.writeFileSync(path.join(distDir, 'gallery-items.html'), galleryHTML);
     console.log('Gallery HTML generated successfully!');
-    console.log('Files copied to dist directory');
 }
 
 // Generate gallery once for deployment
